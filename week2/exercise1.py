96156dd44d5edab6850206965c5dd785fc400915
Shawn <shawnteww@gmail.com>
2020-06-10 11:22:12 +0800

    changes to ex1

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -10,26 +10,26 @@ import platform
 
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
-
+it will create lists from the words
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
-
+it will put the word from the list in the terminal
 for word in some_words:
     print(word)
-
+it will do the same as lines 16-17
 for x in some_words:
     print(x)
-
+it will print the list
 print(some_words)
-
+it will print 'some_words contains more than 3 words' because some words has more than 3 words
 if len(some_words) > 3:
     print('some_words contains more than 3 words')
-
+it creates a function called usefulFunction 
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
-    """
+    """this will print the plantform
     print(platform.uname())
 
 usefulFunction()
